2f398164d7f190e37c647d817171cb2907656432
Theo2801 <theologos.bourloglou@gmail.com>
2024-02-11 19:35:17 +0200

    [feat] Integrate product page component, product page design

--- app.js
+++ app.js
@@ -17,4 +17,42 @@ const fetchComponents = (components) => {
         });
     });
   };
-  
+
+
+/**
+ * Extracts the value of the 'product' URL parameter from the current window's URL.
+ *
+ * @returns {string|null} The value of the 'product' URL parameter if found, otherwise null.
+ */
+const getProductFromURL = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get('product');
+}
+
+const fetchProduct = () => {
+  return {
+      loading: true,
+      urlSlug: null,
+      product: {},
+      loadData() {
+          fetch('products.json') // Sample API endpoint
+              .then(response => {
+                  if (!response.ok) {
+                      throw new Error('Network response was not ok');
+                  }
+                  return response.json();
+              })
+              .then(json => {
+                  this.urlSlug = getProductFromURL();
+                  this.product = json.find(product => product.slug === this.urlSlug);
+
+                  this.loading = false;
+              })
+              .catch(error => {
+                  console.error('There has been a problem with your fetch operation:', error);
+                  this.loading = false;
+              });
+      }
+  };
+}
